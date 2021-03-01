0e4c7d96d1512952317ac8a0ece4eb62b7d2e986
varsha_g <varsha.gouthamchand@maastro.nl>
2021-03-01 12:32:17 +0100

    Commiting Dash file

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import requests
 import pandas as pd
-import csv
+from io import StringIO
 
-# This dataframe has 244 lines, but 4 distinct values for `day`
-#df = px.data.tips()
-
-def queryresult():
-    endpoint = "http://docker-graphdb-root-johan-graphdb.app.dsri.unimaas.nl/repositories/Varsha_tcia"
-
+def queryresult(repo):
+    endpoint = "http://docker-graphdb-root-johan-graphdb.app.dsri.unimaas.nl/repositories/"+repo
     query = """
     PREFIX db: <http://localhost/rdf/ontology/>
     PREFIX dbo: <http://um-cds/ontologies/databaseontology/>
@@ -22,34 +18,38 @@ def queryresult():
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
 
-    SELECT ?patient ?IDvalue ?agevalue ?gendervalue ?genderCode ?tstageCode
+    SELECT ?genderCode ?tstageCode ?nstageCode ?survivalCode
 
     WHERE {
-   
-    ?tablerow roo:P100061 ?patientID.
-    ?tablerow roo:hasage ?age. 
+    
     ?tablerow roo:P100018 ?gender.
     ?tablerow roo:P100029 ?neoplasm.
     ?neoplasm roo:P100244 ?tstage.
-  
-    ?patientID roo:P100042 ?IDvalue. 
-    ?age roo:P100042 ?agevalue.
-    ?gender roo:P100042 ?gendervalue.
-    ?tstage roo:P100042 ?tstagevalue.
-    
+    ?neoplasm roo:P100242 ?nstage.
+    #?neoplasm roo:P100241 ?mstage.
+    ?tablerow roo:P100254 ?survival.
+
     BIND(strafter(str(?tablerow), "http://172.20.10.14/rdf/data/") AS ?patient)
- 
+
     OPTIONAL
     {
-        ?gender a ?s.
+        ?gender a ?g.
         ?tstage a ?t.
+        ?nstage a ?n.
+        #?mstage a ?m.
+        ?survival a ?s.
   
-  		#BIND(strafter(str(?tablerow), "http://172.20.10.14/rdf/data/") AS ?patient)
-   		FILTER regex(str(?s), 		("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C16576|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C20197"))
+   		FILTER regex(str(?g), ("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C16576|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C20197"))
         FILTER regex(str(?t), ("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48719|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48720|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48724|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48728|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48732"))
-  		BIND(strafter(str(?s), "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#") AS ?genderCode)
+        FILTER regex(str(?n), ("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48705|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48706|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48786|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48714"))
+        #FILTER regex(str(?m), ("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48699|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C48700"))
+        FILTER regex(str(?s), ("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C28554|http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C37987"))
+  		BIND(strafter(str(?g), "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#") AS ?genderCode)
         BIND(strafter(str(?t), "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#") AS ?tstageCode)
-    
+        BIND(strafter(str(?n), "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#") AS ?nstageCode)
+        #BIND(strafter(str(?m), "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#") AS ?mstageCode)
+        BIND(strafter(str(?s), "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#") AS ?survivalCode)
+
     }
 }
             """
@@ -62,53 +62,53 @@ def queryresult():
                                        })
     output = annotationResponse.text
     # print(annotationResponse.text)
+    #f = open("csv_output.csv", 'w', newline='')
+    #f.write(output)
+    #f.close()
+    return output
 
-    f = open("csv_output.csv", 'w', newline='')
-    f.write(output)
-    f.close()
-
-queryresult()
-
-data = pd.read_csv("csv_output.csv")
+#data = pd.read_csv("csv_output.csv")
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
+app.layout = html.Div(
+     [
     html.P("Columns:"),
     dcc.Dropdown(
         id='columns',
         value='genderCode',
         options=[{'label': 'Gender', 'value': 'genderCode'},
-                 {'label': 'Tstage', 'value': 'tstageCode'}],
-        clearable=False
-    ),
+                 {'label': 'Tstage', 'value': 'tstageCode'},
+                 {'label': 'Nstage', 'value': 'nstageCode'},
+                 {'label': 'Survival Status', 'value': 'survivalCode'}],
+        clearable=False ),
     html.P("Dataset:"),
     dcc.Checklist(
-        id = 'dataset',
+        id='dataset',
         options=[
-        {'label': 'Maastricht', 'value': 'triplifier_test_HN_Maastricht'},
-        {'label': 'Montréal', 'value': 'triplifier_test_HN_Montreal'},
-        {'label': 'Houston', 'value': 'triplifier_test_HN_Houston'},
-        {'label': 'Toronto', 'value': 'triplifier_test_HN_Toronto'}
-    ],
-    value=['triplifier_test_HN_Maastricht'],
-    labelStyle={'display': 'inline-block'}
+            {'label': 'Maastricht', 'value': 'Maastricht'},
+            {'label': 'Montréal', 'value': 'Montreal'},
+            {'label': 'Houston', 'value': 'Houston'},
+            {'label': 'Toronto', 'value': 'Toronto'}
+        ],
+        value=['Maastricht'],
+        labelStyle={'display': 'inline-block'}
     ),
-    dcc.Graph(id="pie-chart"),
-    dcc.Graph(id='Boxplot')
+    dcc.Graph(id="pie-chart")
 ])
 
+
 @app.callback(
-    Output("pie-chart", "figure"), 
+    Output("pie-chart", "figure"),
     [Input("dataset", "value"),
-        Input("columns", "value")])
+     Input("columns", "value")])
 def generate_chart(dataset, columns):
     result = pd.DataFrame()
     for d in dataset:
-        var = data[data['patient'].str.contains(d)]
-        result = result.append(var)
-    #print(result)
-    fig = px.pie(data, names=result[columns], color_discrete_sequence=px.colors.sequential.RdBu)
+        result_data = queryresult(d)
+        data = pd.read_csv(StringIO(result_data))
+        result = result.append(data)
+    fig = px.pie(result, names=result[columns], color_discrete_sequence=px.colors.sequential.RdBu)
     return fig
 
 app.run_server(debug=True)
