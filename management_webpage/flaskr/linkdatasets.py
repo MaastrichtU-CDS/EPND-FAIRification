a44d811530d358e9ba018e9b01c38c74a981f214
brianfred <dols.brian@gmail.com>
2022-03-18 12:56:35 +0100

    Implemented dropdown lists not including already linked values

--- management_webpage/flaskr/linkdatasets.py
+++ management_webpage/flaskr/linkdatasets.py
@@ -1,71 +1,48 @@
 from flask import (
     Blueprint, render_template, request
 )
-
-from flaskr.db import get_db
 from flaskr import useDataset, useCDM, useLinkdata
 import pandas as pd
 import numpy as np
 
 bp = Blueprint("linkdatasets",__name__,url_prefix="/linkdatasets")
 
-
-
-
-
 @bp.route('/linkdatasets', methods=('GET', 'POST'))
 def linker():
-    CDM = getCDM()
-    cdmcol = list(CDM['variable'])
-
-    dataset = getDataset()
-    datacol = list(dataset.columns.values)
+    useDataset.getTest()
 
     links = useLinkdata.getDatabase()
+    linksdf = useLinkdata.getDataframe()
 
-    df1 = useDataset.getDatasetNames()
-    df2 = useCDM.getDataframe()
-    df3 = useLinkdata.getDataframe()
+    dataset = getDatasetNames()
+    dataset = dataset[~(dataset.isin(linksdf['datacolumn']))].reset_index(drop=True)
+    datacol = dataset
 
-    #a = datacol.difference(df3.datacolumn)
-    #df1_notin2 = df1[~(df1.isin(df2['variable']))].reset_index(drop=True)
-    df_1notin3 = df1[~(df1.isin(df3['datacolumn']))].reset_index(drop=True)
-    #df_1notin3 = df3[~(df3['datacolumn'].isin(df1))].reset_index(drop=True)
-    df_2notin3 = df2[~(df2['variable'].isin(df3['cdmcolumn']))].reset_index(drop=True)
-    #df_1notin3 = datacol[~(datacol.isin(df3['cdmcolumn']))].reset_index(drop=True)
-    print(df_1notin3)
-    print("tussen1")
-    print(df1)
-    #print("tussen2")
-    #print(df1_notin2)
-    #print("tussen3")
-    #print(df1)
-
-    #print("CDM")
-    #print(df2["variable"][28])
-    #print(df3["cdmcolumn"][5])
-    #print('dataset')
-    #print(df1[5])
-    #print(df3["datacolumn"][6])
-
+    CDM = getCDM()
+    CDM = CDM[~(CDM['variable'].isin(linksdf['cdmcolumn']))].reset_index(drop=True)
+    cdmcol = list(CDM['variable'])
     
     return render_template("linkdatasets/linkdatasets.html", links=links, columns=datacol, cdmcolumns=cdmcol)
     #return render_template("linkdatasets/linkdatasets.html", links=links, columns=datacol, cdmcolumns=cdmcol, CDMtables=[CDM.to_html(classes='data')], CDMtitles=CDM.columns.values, tables=[datadescribe.to_html(classes='data')], titles=datadescribe.columns.values)
 
 @bp.route('/submit-form', methods = ['GET', 'POST'])
 def submitForm():
-    CDM = getCDM()
-    cdmcol = list(CDM['variable'])
-
-    dataset = getDataset()
-    datacol = list(dataset.columns.values)
-
     selectValueData = request.form.get('columns')
     selectValueCDM = request.form.get('cdmcolumns')
     print(selectValueData)
     print(selectValueCDM)
     useLinkdata.newLink(selectValueData, selectValueCDM)
+
     links = useLinkdata.getDatabase()
+    linksdf = useLinkdata.getDataframe()
+
+    dataset = getDatasetNames()
+    dataset = dataset[~(dataset.isin(linksdf['datacolumn']))].reset_index(drop=True)
+    datacol = dataset
+
+    CDM = getCDM()
+    CDM = CDM[~(CDM['variable'].isin(linksdf['cdmcolumn']))].reset_index(drop=True)
+    cdmcol = list(CDM['variable'])
 
     return render_template("linkdatasets/linkdatasets.html", links=links, columns=datacol, cdmcolumns=cdmcol)
     #return render_template("linkdatasets/linkdatasets.html", links=links, columns=datacol, cdmcolumns=cdmcol, CDMtables=[CDM.to_html(classes='data')], CDMtitles=CDM.columns.values, tables=[datadescribe.to_html(classes='data')], titles=datadescribe.columns.values)
@@ -82,17 +59,25 @@ def getDataset():
     dataset = useDataset.getDataframe()
     return dataset
 
+def getDatasetNames():
+    dataset = useDataset.getDatasetNames()
+    return dataset
+
 @bp.route('/<string:name>/deleteLink', methods=('POST',))
 def deleteLink(name):
     useLinkdata.deleteLink(name)
 
+    links = useLinkdata.getDatabase()
+    linksdf = useLinkdata.getDataframe()
+
+    dataset = getDatasetNames()
+    dataset = dataset[~(dataset.isin(linksdf['datacolumn']))].reset_index(drop=True)
+    datacol = dataset
+
     CDM = getCDM()
+    CDM = CDM[~(CDM['variable'].isin(linksdf['cdmcolumn']))].reset_index(drop=True)
     cdmcol = list(CDM['variable'])
 
-    dataset = getDataset()
-    datacol = list(dataset.columns.values)
-
-    links = useLinkdata.getDatabase()
 
     return render_template("linkdatasets/linkdatasets.html", links=links, columns=datacol, cdmcolumns=cdmcol)
     #return render_template("linkdatasets/linkdatasets.html", links=links, columns=datacol, cdmcolumns=cdmcol, CDMtables=[CDM.to_html(classes='data')], CDMtitles=CDM.columns.values, tables=[datadescribe.to_html(classes='data')], titles=datadescribe.columns.values)
--- management_webpage/flaskr/useDataset.py
+++ management_webpage/flaskr/useDataset.py
@@ -1,5 +1,6 @@
 from flaskr.db import get_db
 import pandas as pd
+import sparql_dataframe as sd
 
 def getTableInfo():
     db = get_db()
@@ -26,3 +27,22 @@ def getDatasetNames():
     df = pd.read_sql_query('PRAGMA table_info(dataset)', db)
     df = df['name']
     return df
+
+def getTest():
+    endpoint = 'https://graphdb.jvsoest.eu/repositories/epnd_dummy'
+
+    q = """
+    PREFIX dbo: <http://um-cds/ontologies/databaseontology/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+    select ?colUri ?columnName ?tableName 
+    where { 
+	    ?colUri rdfs:subClassOf dbo:ColumnCell;
+            dbo:column ?columnName;
+            dbo:table ?tableUri.
+   	    ?tableUri dbo:table ?tableName.
+    }
+    """
+
+    df = sd.get(endpoint, q)
+    print(df)
