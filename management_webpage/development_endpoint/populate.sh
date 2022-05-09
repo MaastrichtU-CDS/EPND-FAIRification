791a5f5cb8cbdfaa170731bee8cb43ac66c4c348
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2022-05-09 16:36:53 +0200

    Updated the local development endpoint population script

--- management_webpage/development_endpoint/populate.sh
+++
@@ -1,7 +0,0 @@
-#curl -X POST --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F @config.ttl http://localhost:7200/rest/repositories
-
-curl -d @../shapeTest/shacl.ttl --header "Content-Type: application/x-turtle" http://localhost:7200/repositories/epnd_dummy/statements?context=%3Chttp://shacl.local/%3E
-
-curl -d @ontology.owl --header "Content-Type: application/rdf+xml" http://localhost:7200/repositories/epnd_dummy/statements?context=%3Chttp://ontology.local/%3E
-
-curl -d @output.ttl --header "Content-Type: application/x-turtle" http://localhost:7200/repositories/epnd_dummy/statements?context=%3Chttp://data.local/%3E
---
+++ management_webpage/development_endpoint/run.sh
@@ -0,0 +1,23 @@
+docker-compose up -d
+
+echo "Waiting for GraphDB to be up-and-running"
+curl -X GET --header 'Accept: application/json' 'http://localhost:7200/rest/locations'
+retVal=$?
+while [ $retVal -ne 0 ]
+do
+    sleep 5
+    curl -X GET --header 'Accept: application/json' 'http://localhost:7200/rest/locations'
+    retVal=$?
+done
+
+# Create repository from config
+curl -X POST -F config=@config.ttl --header 'Content-Type: multipart/form-data' --header 'Accept: */*' 'http://localhost:7200/rest/repositories'
+
+# Upload SHACL file
+curl -d @../shapeTest/shacl.ttl --header "Content-Type: application/x-turtle" http://localhost:7200/repositories/epnd_dummy/statements?context=%3Chttp://shacl.local/%3E
+
+# Upload ontology file
+curl -d @ontology.owl --header "Content-Type: application/rdf+xml" http://localhost:7200/repositories/epnd_dummy/statements?context=%3Chttp://ontology.local/%3E
+
+# Upload data file
+curl -d @output.ttl --header "Content-Type: application/x-turtle" http://localhost:7200/repositories/epnd_dummy/statements?context=%3Chttp://data.local/%3E
