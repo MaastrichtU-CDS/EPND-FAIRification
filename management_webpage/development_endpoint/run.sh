86df78193da6d69ae74c4b18f1b8290ccd632ca4
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2022-05-12 12:09:20 +0200

    Fixed sed

--- management_webpage/development_endpoint/run.sh
+++ management_webpage/development_endpoint/run.sh
@@ -1,3 +1,10 @@
+os=$(uname -s)
+
+sed=sed
+if [[ $os -eq "Darwin" ]]; then
+    sed=gsed
+fi
+
 # docker-compose up -d
 
 # echo "Waiting for GraphDB to be up-and-running"
@@ -14,7 +21,7 @@
 # curl -X POST -F config=@config.ttl --header 'Content-Type: multipart/form-data' --header 'Accept: */*' 'http://localhost:7200/rest/repositories'
 
 # Upload SHACL file
-sed "s/^\s*#.*$//g" ../shapeTest/shacl.ttl > shacl.ttl
+$sed "s/^\s*#.*$//g" ../shapeTest/shacl.ttl > shacl.ttl
 curl -d @shacl.ttl --header "Content-Type: application/x-turtle" http://localhost:7200/repositories/epnd_dummy/statements?context=%3Chttp://shacl.local/%3E
 rm shacl.ttl
 
