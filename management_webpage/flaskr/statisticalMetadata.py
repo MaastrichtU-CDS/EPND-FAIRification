bf120bd71fbca7756bf6a4b0eed3443d3118855c
Shashank Chakravarthy <shashank@shashanks-mbp.wired.unimaas.local>
2022-05-16 14:58:20 +0200

    Adding categorical column

--- management_webpage/flaskr/statisticalMetadata.py
+++ management_webpage/flaskr/statisticalMetadata.py
@@ -2,10 +2,13 @@ import pandas as pd
 
 #Create Numeric/Unknown Metadata
 def numericMetadata(df):
+    dfMetadata = pd.DataFrame()
     dfMetadata = df.describe()
     return dfMetadata
 
 #Create Categorical Metadata
 def categoricalMetadata(df):
-    dfMetadata = df['cellValue'].value_counts(normalize=True)
+    dfMetadata = pd.DataFrame()
+    dfMetadata['categoricalValue'] = df['cellValue'].unique()
+    dfMetadata['cellValue'] = df['cellValue'].value_counts(normalize=True)
     return dfMetadata
