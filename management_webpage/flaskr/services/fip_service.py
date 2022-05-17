a616ca620e1625a33ed7acb3cb26f112abe2f804
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2022-05-17 14:39:53 +0200

    Visualize shacl location

--- management_webpage/flaskr/services/fip_service.py
+++ management_webpage/flaskr/services/fip_service.py
@@ -12,13 +12,17 @@ class FipService:
         return self.__triplestore.select_sparql("""
             prefix fip: <https://w3id.org/fair/fip/terms/>
             prefix sio: <http://semanticscience.org/resource/>
+            prefix sh: <http://www.w3.org/ns/shacl#>
             
-            SELECT DISTINCT ?fip ?fip_label ?cedar_template
+            SELECT DISTINCT ?fip ?fip_label ?cedar_template ?shacl_location
             WHERE {
                 ?fip rdf:type fip:FIP-Declaration;
                     fip:declares-current-use-of [
                         rdf:type <https://schema.metadatacenter.org/core/Template>;
                         sio:SIO_000628 ?cedar_template;
+                    ];
+                    fip:declares-current-use-of [
+                        sh:shapesGraph ?shacl_location;
                     ].
                 OPTIONAL { ?fip rdfs:label ?fip_label; }
             }""")
