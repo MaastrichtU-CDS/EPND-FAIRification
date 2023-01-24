e76b67030669a4ac2cec4a9eab1b7b203cb46e8f
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-01-24 13:41:57 +0100

    Moved run script outside of container (and added json handling)

---
+++ ADWB_container/container_run.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+/opt/graphdb/dist/bin/graphdb -Dgraphdb.home=/opt/graphdb/home -Dorg.xml.sax.driver=com.sun.org.apache.xerces.internal.parsers.SAXParser -Djdk.xml.entityExpansionLimit=1000000 & > /graphdb_stdout.log 2> /graphdb_stderr.log
+sleep 60
+
+cd /triplifier-boot && java -jar triplifier-boot.jar & > /triplifier_stdout.log 2> /triplifier_stderr.log
+cd /app
+
+# Set triplifier location
+if [ -z "$TRIPLIFIER_LOCATION" ]; then
+    TRIPLIFIER_LOCATION=cat /app/config.json | jq '.triplifier_service'
+    export TRIPLIFIER_LOCATION
+fi
+cat config.json | jq '.triplifier_service = "$TRIPLIFIER_LOCATION"' > /app/config.json
+
+FLASK_APP=flaskr
+FLASK_ENV=development
+export FLASK_APP=$FLASK_APP
+export FLASK_ENV=$FLASK_ENV
+flask run -h 0.0.0.0 -p 5000
