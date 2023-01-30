a80d3b888b82c93f0cf76dedd140c72fba5d569f
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-01-30 12:38:21 +0100

    Cleaning up node_modules etc.

--- ADWB_container/run.sh
+++ ADWB_container/run.sh
@@ -6,7 +6,6 @@ docker run --rm -it \
     -v $(pwd)/build_triplifier_boot.sh:/build.sh \
     --workdir / \
     maven:3.8-eclipse-temurin-11 /bin/bash build.sh
-sudo mv triplifier/triplifier-boot-0.0.1-SNAPSHOT.jar ../triplifier-boot-0.0.1-SNAPSHOT.jar
 
 ## Do final container build
 sh run_build_short.sh
--- ADWB_container/run_build_short.sh
+++ ADWB_container/run_build_short.sh
@@ -1,6 +1,10 @@
 ## Copy webapp folder here
 cp -R ../management_webpage ./app
 
+rm -Rf app/cedar_embeddable_editor/cedar-embeddable-editor-release-2.6.18/node_modules
+rm -Rf app/node_modules
+rm -Rf app/venv
+
 ## build the actual container
 docker build -t jvsoest/adwb --no-cache ./
 
