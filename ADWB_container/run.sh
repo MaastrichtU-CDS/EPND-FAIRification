c72763679dce7a48c14770372e6c9aa59b02aa22
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-01-30 11:40:11 +0100

    Updated build script to download and build triplifier WITHIN container to reduce volume latency issues

--- ADWB_container/run.sh
+++ ADWB_container/run.sh
@@ -1,15 +1,12 @@
 ## build triplifier
-sudo rm -f triplifier*.jar # clean old build first
+sudo rm -Rf triplifier/ # clean old build first
 
-git clone --recurse-submodules https://github.com/MaastrichtU-CDS/triplifier-boot
-cd triplifier-boot
 docker run --rm -it \
-    -v $(pwd):/triplifier-boot \
-    -v $(pwd)/../build_triplifier_boot.sh:/triplifier-boot/build.sh \
-    --workdir /triplifier-boot \
+    -v $(pwd)/triplifier:/output \
+    -v $(pwd)/build_triplifier_boot.sh:/build.sh \
+    --workdir / \
     maven:3.8-eclipse-temurin-11 /bin/bash build.sh
-sudo mv target/triplifier-boot-0.0.1-SNAPSHOT.jar ../triplifier-boot-0.0.1-SNAPSHOT.jar
-cd ../ && sudo rm -Rf triplifier-boot
+sudo mv triplifier/triplifier-boot-0.0.1-SNAPSHOT.jar ../triplifier-boot-0.0.1-SNAPSHOT.jar
 
 ## Do final container build
 sh run_build_short.sh
