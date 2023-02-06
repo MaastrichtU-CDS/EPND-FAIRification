da863742df6cf9f85434d7b49bb8cc363b1c2c88
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-02-06 15:17:38 +0100

    Now replacing env vars at the right location

--- ADWB_container/container_run.sh
+++ ADWB_container/container_run.sh
@@ -3,15 +3,13 @@
 /opt/graphdb/dist/bin/graphdb -Dgraphdb.home=/opt/graphdb/home -Dorg.xml.sax.driver=com.sun.org.apache.xerces.internal.parsers.SAXParser -Djdk.xml.entityExpansionLimit=1000000 & > /graphdb_stdout.log 2> /graphdb_stderr.log
 sleep 60
 
+# Set triplifier location
+python replace_env.py
+
 cd /triplifier-boot && java -jar triplifier-boot.jar & > /triplifier_stdout.log 2> /triplifier_stderr.log
 cd /app
 
 sleep 20
-
-# Set triplifier location
-python replace_env.py
-
-sleep 5
 
 FLASK_APP=flaskr
 FLASK_ENV=development
