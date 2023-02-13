0d9731c8fea7c63b7bb0043db4b3b29b539d7945
Shashank <shashankmc@github.com>
2023-02-13 13:52:15 +0100

    Removed default value, corrected return fip value, added new method in class to handle fetch connection refused error

--- management_webpage/flaskr/services/nanopub_service.py
+++ management_webpage/flaskr/services/nanopub_service.py
@@ -6,22 +6,27 @@ class NanopubService:
     def __init__(self, uri):
         self.uri = uri
 
+    def fetch_np(self, np): 
+        try:
+            np_parsed = Nanopub(np)
+        except ConnectionError as e:
+            raise Exception("Connection refused by server,\
+                    please try after a while")
+        return np_parsed
+
     def parse_shacl_uri(self):
-        np = Nanopub(self.uri)
+        np = self.fetch_np(self.uri)
         for s, p, o in np.assertion:
-            for s1, p1, o1 in Nanopub(o).assertion:
+            for s1, p1, o1 in self.fetch_np(o).assertion:
                 if o1 ==\
                 URIRef("https://w3id.org/fair/fip/terms/FIP-Question-I2-D")\
                 and\
                 p1 ==\
                 URIRef("https://w3id.org/fair/fip/terms/refers-to-question"):
-                    for s2, p2, o2 in Nanopub(o).assertion:
+                    for s2, p2, o2 in self.fetch_np(o).assertion:
                         if p2 ==\
                         URIRef("https://w3id.org/fair/fip/terms/declares-planned-use-of"):
-                            for s3, p3, o3 in Nanopub(re.split('#',
-                                                               o2)[0]).assertion:
-                                if p3 ==\
-                                URIRef("http://www.w3.org/2000/01/rdf-schema#seeAlso"):
-                                    #return o1
-                                    return \
-                                "https://raw.githubusercontent.com/MaastrichtU-CDS/EPND-FAIRification/main/fip/fip.ttl"
+                            o2_parsed = re.split('#', o2)[0]
+                            for s3, p3, o3 in self.fetch_np(o2_parsed).assertion:
+                                if p3 == URIRef("http://www.w3.org/2000/01/rdf-schema#seeAlso"):
+                                    return o3 
